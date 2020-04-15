aca9f55bf8ff70a81d903cb61ade2b89e53dc264
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-04-15 23:30:15 +0600

    fronend completed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Navbar from './components/navbar'
+import ExerciseList from './components/exercise-list'
+import EditExercise from './components/edit-exercise'
+import CreateExercise from './components/create-exercise'
+import CreateUser from './components/create-user'
+
 function App() {
   return (
     <Router>
-      <Navbar></Navbar>
-      <br></br>
+      <div className="container">
+        <Navbar></Navbar>
+        <br></br>
 
-      <Route path="/" exact component= {ExerciseList}></Route>
-      <Route path="/edit/:id" exact component= {EditExercise}></Route>
-      <Route path="/create" exact component= {CreateExercise}></Route>
-      <Route path="/user" exact component= {CreateUser}></Route>
+        <Route path="/" exact component= {ExerciseList}></Route>
+        <Route path="/edit/:id" exact component= {EditExercise}></Route>
+        <Route path="/create" exact component= {CreateExercise}></Route>
+        <Route path="/user" exact component= {CreateUser}></Route>
+      </div>
     </Router>
 
   );
